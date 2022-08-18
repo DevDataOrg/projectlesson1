ff2a75f58b0c493a642341749d29978b19196293
DevDataOrg <pmbiclub@gmail.com>
2022-08-18 09:52:58 +0300

    first message

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+
+alert(1234);
+console.log(12345);
+
+let number = 5;
+const leftBorderWidth = 1; 
+
+alert(number);
+console.log(number=566565556);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log(obj);
+
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
+
+
+const result = confirm('A you hear');
+console.log(result);
+
+const answer = prompt("Вам есть 18?", "18");
+//const answer = +prompt("Вам есть 18?", "18");
+
+console.log(answer);
+
+const answers = [];
+answers[0] = prompt('Как ваше имя', '');
+answers[1] = prompt('Как ваше фамилия', '');
+answers[2] = prompt('Сколько вам лет', '');
+
+document.write(answers);
