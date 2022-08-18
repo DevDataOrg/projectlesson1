66c0af0070ef1ef1aa796b84fe31d63e78aaae16
DevDataOrg <pmbiclub@gmail.com>
2022-08-18 13:26:51 +0300

    6  message

--- js/script.js
+++ js/script.js
@@ -38,4 +38,4 @@ answers[0] = prompt('Как ваше имя', '');
 answers[1] = prompt('Как ваше фамилия', '');
 answers[2] = prompt('Сколько вам лет', '');
 
-document.write(answers);
+// document.write(answers);
